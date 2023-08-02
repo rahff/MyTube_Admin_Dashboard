53f39272de745eab62343ffe2091e4ccdda21c3a
rahff <raphaelandrey99@gmail.com>
2023-08-02 11:14:11 +0200

    module config created

---
+++ GUI/src/dashboard/config/config.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ConfigRoutingModule } from './config-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    ConfigRoutingModule
+  ]
+})
+export class ConfigModule { }
